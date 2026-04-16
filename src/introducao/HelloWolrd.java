959b8abd87c6a8e73655857657e26db8a5c10d7f
Carolina Perpetuo <carolinaperpetuocosta@gmail.com>
2026-04-16 16:59:52 -0300

    Inicializando meu Projeto

---
+++ src/introducao/HelloWolrd.java
@@ -0,0 +1,13 @@
+package introducao;
+
+public class HelloWolrd {
+
+	public static void main(String[] args) {	//metodo principal que executa codigo
+		
+		//Nossa logica aqui
+		
+		System.out.print("Hello World"); 	//identico ao escreva do portugol
+  
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,28 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Variaveis 
+		int idade = 25;
+		String nome = "Carolina";
+		float graus = 20.0f;    // float -> ponto flutuante
+		boolean respostas;  // boolean-> logico true/false
+		
+		
+		
+		// Constantes
+		int QUANTIDADE = 50;
+		String TURAMA = "Turma 84";
+		double AREA = 3.1415;
+		
+		System.out.print(nome);
+		System.out.println(QUANTIDADE);
+		System.out.println(graus);
+		
+		
+	}
+
+}
